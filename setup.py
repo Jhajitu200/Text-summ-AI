a9f334594f6aa366730ebf32e94e7db0400cbb07
Jitu Kumar <jitujha2002@gmail.com>
2024-05-14 11:53:13 +0530

    setup file changed

--- setup.py
+++ setup.py
@@ -7,9 +7,9 @@ with open("README.md", "r", encoding="utf-8") as f:
 __version__ = "0.0.0"
 
 REPO_NAME = "Text-Summarizer-Project"
-AUTHOR_USER_NAME = "entbappy"
+AUTHOR_USER_NAME = "Jhajitu200"
 SRC_REPO = "textSummarizer"
-AUTHOR_EMAIL = "entbappy73@gmail.com"
+AUTHOR_EMAIL = "jitujha2002@gmail.com"
 
 
 
